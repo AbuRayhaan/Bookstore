f954484560985916398d110a847f3d11d0539574
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-04 07:31:26 +0100

    Modified the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Pages from './components/books/Pages';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 const App = () => {
   return (
-    <div className="App">
-      <Pages />
-    </div>
-  );
+    <>
+      <Routes>
+        <Route exact path="/" element={<Books />} />;
+        <Route path ="/categories" element={<Categories />} />;
+      </Routes>
+    </>
+  )
 }
 
 export default App;
