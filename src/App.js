6de5a55995e0a159f7076fe68a6e940e1731a495
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-04 07:52:07 +0100

    Modified the project files to fix linters error

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
 
-const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route exact path="/" element={<Books />} />;
-        <Route path ="/categories" element={<Categories />} />;
-      </Routes>
-    </>
-  )
-}
+const App = () => (
+  <>
+    <Routes>
+      <Route exact path="/" element={<Books />} />
+      ;
+      <Route path="/categories" element={<Categories />} />
+      ;
+    </Routes>
+  </>
+);
 
 export default App;
